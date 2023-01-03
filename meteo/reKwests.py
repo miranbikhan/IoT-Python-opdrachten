14fe78845e21ed76e7657c4e74c38fca4c819e61
Miran Bikhan <miran.bikhan@student.ap.be>
2023-01-03 02:47:14 +0100

    Begonnen met JSON en Requests enzo.

---
+++ meteo/reKwests.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+api_url = "https://api.open-meteo.com/v1/forecast?"
+
+apen = "latitude=51.22&longitude=4.40&hourly=temperature_2m"
+snt_mnca = "latitude=34.02&longitude=-118.49&hourly=temperature_2m"
+tkyo = "latitude=35.69&longitude=139.69&hourly=temperature_2m"
+
+# api = requests.get("https://open-meteo.com/en/docs#api_form")
+
+
+# city = input("give me a city ")
+
+# response = requests.post(url, data={'select_city': api})
+
+# print("antwerpen" in response.text)
+    
+url = requests.get(api_url + apen)
+
+weather_info = url.json()
+
+print(weather_info)
